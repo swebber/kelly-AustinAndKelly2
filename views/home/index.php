96a62003f83c3a6036d1532be423257bad06a8b5
swebber <scott@teagan.org>
2016-11-06 21:38:08 -0500

    final content roughed in

--- views/home/index.php
+++ views/home/index.php
@@ -37,6 +37,90 @@
         </div>
     </section>
     <!-- end header -->
+    <!-- rsvp section -->
+    <section id='rsvp'>
+        <div class="container">
+            <h1 class="rsvp-title">RSVP</h1>
+            <p class="rsvp-about">Please RSVP by May 25th</p>
+            <form id="rsvpForm" method="post" action="/rsvp">
+                <div class="row">
+                    <div class="col-sm-1 col-md-2 hidden-xs"></div>
+                    <div class="col-xs-12 col-sm-5 col-md-4">
+                        <div class="form-group">
+                            <label class="control-label" for="guestNames">Guest Name(s)</label>
+                            <input type="text" class="form-control" id="guestNames" data-error="Please tell us your names." required></input>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-5 col-md-4">
+                        <div class="form-group">
+                            <label class="control-label" for="willAttend">Will You Be Attending?</label>
+                            <select class="form-control" id="willAttend" data-error="Let us know if you will be attending." required>
+                                <option value="">Select ...</option>
+                                <option value="1">Yes of course!</option>
+                                <option value="0">No I am unable to attend.</option>   
+                            </select>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-1 col-md-2 hidden-xs"></div>
+                    <div class="col-xs-12 col-sm-5 col-md-4">
+                        <div class="form-group">
+                            <label class="control-label" for="needTrans">Will You Need Transportation?</label>
+                            <select class="form-control" id="needTrans" data-odd="odd">
+                                <option value="NA">Select ...</option>
+                                <option value="1">Yes</option>
+                                <option value="0">No</option>   
+                            </select>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-5 col-md-4">
+                        <div class="form-group">
+                            <label for="dietInfo">Dietary Restrictions</label>
+                            <input type="text" class="form-control" id="dietInfo" name="dietInfo"></input>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-1 col-md-2 hidden-xs"></div>
+                    <div class="col-xs-12 col-sm-10">
+                        <div class="form-group">
+                            <input type="hidden" value="NA" id="notRobot" data-captchacheck="captchacheck" data-validate="true" />
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-1 col-md-2 hidden-xs"></div>
+                    <div class="col-xs-12 col-sm-10">
+                        <input type="submit" class="btn btn-default" value="Submit">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+    <!-- end rsvp section -->
+    <!-- about section -->
+    <section id="about">
+        <div class="container">
+            <h1>ABOUT THE WEDDING</h1>
+            <p class="about-address">
+                <b>THE ANCHOR INN</b><br />
+                7617 WATER OAK POINT ROAD<br />
+                PASADENA, MARYLAND 21122
+            </p>
+            <p class="about-venue">
+                We are getting married at the Anchor Inn on June 16th at 6:00 PM.
+                The ceremony and reception are both taking place at the same venue and
+                will be both indoors and outdoors. For those of you bringing small children please
+                be mindful as the venue is on the water.
+            </p>
+        </div>
+    </section>
+    <!-- end about section -->
     <!-- lodging and transportation section -->
     <section id="lodging-transportation">
         <div class="container">
