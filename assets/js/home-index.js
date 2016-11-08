b5b565add21e437b13473a7006fbe9d6e8bb88f5
swebber <scott@teagan.org>
2016-11-08 10:51:01 -0500

    I've got easing working.

--- assets/js/home-index.js
+++ assets/js/home-index.js
@@ -1,2 +1,19 @@
 $(document).ready(function () {
+    $('#navbar li').bind('click', function(e) {
+        var anchor = $(this).find('a');
+        $('#navbar li').removeClass('active');
+        $(this).addClass('active');
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top + 10
+        }, 1500, 'easeInOutExpo');
+        e.preventDefault();
+    });
+
+    $('.navbar-brand').bind('click', function(e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        e.preventDefault();
+    });
 });
--- views/layout.php
+++ views/layout.php
@@ -20,6 +20,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="assets/js/ie10-viewport-bug-workaround.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.easing.1.3.js" type="text/javascript"></script>
         <script src="assets/js/home-index.js" type="text/javascript"></script>
     </body>
 </html>
